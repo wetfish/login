9b78b75799174ba04ac3180ed6fe3f2696e25385
Rachel Fish <itsrachelfish@gmail.com>
2014-10-12 01:28:07 -0600

    Woah, partial support!!

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,13 @@ module.exports = function(app)
     app.get('/', function(req, res)
     {
         console.log("Home page loaded!");
-        res.render('index', { title: 'Wetfish!', message: 'Hello World :)' });
+        res.render('index', {
+            title: 'Wetfish!',
+            message: 'Hello World :)',
+            partials: {
+                head: 'partials/head',
+                foot: 'partials/foot'
+            }
+        });
     });
 }
