93b2f9b2f71ee2e8ee00201b6a3f42a74c3ead0b
Rachel Fish <itsrachelfish@gmail.com>
2014-10-15 23:51:30 -0600

    Both devnill and frob complained about my smelly copypasta

--- server/model.js
+++ server/model.js
@@ -35,22 +35,22 @@ var model = {
     connect: function(callback)
     {
         // Create connections for each of the redis databases we use
-        client[0] = redis.createClient(6301);   // Express session store
-        client[1] = redis.createClient(6301);   // Member account data
-        client[2] = redis.createClient(6301);   // Username -> Member lookup
-        client[3] = redis.createClient(6301);   // Email -> Member lookup
-        client[4] = redis.createClient(6301);   // Email token -> Member lookup
+        // 0: Express session store
+        // 1: Member account data
+        // 2: Username -> Member lookup
+        // 3: Email -> Member lookup
+        // 4: Email token -> Member lookup
+
+        var select = [];
+
+        for(i = 0; i < 5; i++)
+        {
+            client[i] = redis.createClient(6301);
+            select[i] = model.async(client[i], client[i].select, [i]);
+        }
         
         // Select databases for each for our connections
-        async.parallel([
-            model.async(client[0], client[0].select, [0]),
-            model.async(client[1], client[1].select, [1]),
-            model.async(client[2], client[2].select, [2]),
-            model.async(client[3], client[3].select, [3]),
-            model.async(client[4], client[4].select, [4])
-        ],
-
-        function(error, message)
+        async.parallel(select, function(error, message)
         {
             callback(error, message);
         });
