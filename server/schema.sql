794f0afcfacc1cf6c43fcd2a1c1ba76fa842bbc8
Rachel Fish <itsrachelfish@gmail.com>
2014-10-22 23:21:30 -0600

    Table schema for users

---
+++ server/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` varchar(64) character set utf8 NOT NULL,
+  `user_name` varchar(32) character set utf8 NOT NULL,
+  `user_email` varchar(256) character set utf8 NOT NULL,
+  `user_password` varchar(64) character set utf8 NOT NULL,
+  `user_token` varchar(64) character set utf8 NOT NULL,
+  `user_verified` tinyint(1) NOT NULL,
+  `user_data` text character set utf8 NOT NULL,
+  UNIQUE KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
