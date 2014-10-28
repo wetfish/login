0fdbb6e97d673dbc047ae58e82697a208773f7b4
Rachel Fish <itsrachelfish@gmail.com>
2014-10-28 00:25:41 -0600

    Rewrote routes to use array map

--- server/server.js
+++ server/server.js
@@ -16,9 +16,6 @@ var app = express();
 // Connect to redis and MySQL
 model.connect(config);
 
-// TODO: Create custom middleware which checks to see if a user is logged in, if so, get the most recent version of their profile from MySQL
-// TODO: Store the last time their profile was edited in redis, check against that instead of just getting it every time (expire after some amount of time)
-
 // Use the existing connection for session data
 app.use(session({
     store: new RedisStore({client: model.redis}),
@@ -34,24 +31,25 @@ app.set('view engine', 'hjs');
 
 // Required variables routes need access to
 var required = {app: app, model: model, sendgrid: sendgrid};
+var routes =
+[
+    'index', 'register', 'verify',
+    'login', 'logout', 'profile',
+    'apps/list', 'apps/create', 'apps/edit',
+    'apps/join', 'apps/token', 'apps/manage',
+    'apps/verify', 'apps/leave'
+];
 
-require('./routes/index.js')(required);
-require('./routes/register.js')(required);
-require('./routes/verify.js')(required);
-require('./routes/login.js')(required);
-require('./routes/profile.js')(required);
-require('./routes/logout.js')(required);
-require('./routes/apps/list.js')(required);
-require('./routes/apps/create.js')(required);
-require('./routes/apps/edit.js')(required);
-require('./routes/apps/join.js')(required);
-require('./routes/apps/leave.js')(required);
-require('./routes/apps/verify.js')(required);
+routes.map(function(route)
+{
+    require('./routes/'+route+'.js')(required);
+});
 
 app.use(express.static(path.join(__dirname, 'static')));
 
 // Redirect to the home page on 404
-app.use(function(req, res, next){
+app.use(function(req, res, next)
+{
     res.redirect('/');
 });
 
