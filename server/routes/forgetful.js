6c7c01d6f367721bb204edcb10b3685d910ff553
Rachel Fish <itsrachelfish@gmail.com>
2015-03-05 23:58:20 -0700

    Setup post request handler to check if accounts exist

--- server/routes/forgetful.js
+++ server/routes/forgetful.js
@@ -1,12 +1,14 @@
+var validator = require('validator');
 var app;
 
 module.exports = function(required)
 {
+    client = required.client;
+    model = required.model;
     app = required.app;
     
     app.get('/forgetful', function(req, res)
     {
-        console.log("GET: /");
         res.render('forgetful', {
             title: 'Forgot your password?',
             user: req.session.user,
@@ -16,5 +18,24 @@ module.exports = function(required)
                 foot: 'partials/foot'
             }
         });
+    });
+
+    app.post('/forgetful', function(req, res)
+    {
+        var account = validator.escape(req.body.forgetful);
+        var select = {};
+
+        // Check if the requested account is an email
+        if(validator.isEmail(account))
+            select['user_email'] = account;
+        else
+            select['user_name'] = account;
+
+        model.user.get(select, function(error, response)
+        {
+            console.log(error, response);
+        });
+        
+        res.end();
     });
 }
